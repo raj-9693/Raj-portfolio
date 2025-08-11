7d666883af3f4c99b531200f36efc4bf3392bb24
RAJ KUMAR NISHAD <rajkumarnishad96930@gmail.com>
2025-08-11 20:10:33 +0530

    Fix image path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import { tempo } from "tempo-devtools/dist/vite";
 
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   base: "/Raj-portfolio",
   optimizeDeps: {
