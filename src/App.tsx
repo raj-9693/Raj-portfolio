b0cff5f3722f76ce9c73e2324741727d9bc39a6a
RAJ KUMAR NISHAD <rajkumarnishad96930@gmail.com>
2025-12-17 13:33:42 +0530

    Remove tempo routes completely

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
 import { Suspense } from "react";
-import { useRoutes, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/home";
-import routes from "tempo-routes";
 
 function App() {
-  // Always call useRoutes unconditionally to follow React hooks rules
-  const tempoRoutes = import.meta.env.VITE_TEMPO === "true" ? useRoutes(routes) : null;
-  
   return (
     <Suspense fallback={<p>Loading...</p>}>
-      <>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-        {tempoRoutes}
-      </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Suspense>
   );
 }
 
 export default App;
+
